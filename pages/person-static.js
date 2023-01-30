94406eb04e8db610c8a74e6eca597cb268ea430e
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-01-30 22:14:59 +0000

    person static

---
+++ pages/person-static.js
@@ -0,0 +1,15 @@
+export default function PersonStatic({ person }) {
+  return (
+    <>
+      Hello {person.firstName} {person.lastName}!
+    </>
+  )
+}
+
+export async function getStaticProps(context) {
+  return {
+    props: {
+      person: { firstName: "John", lastName: "Doe" }
+    }, // will be passed to the page component as props
+  }
+}
