efcb904cc3f28c52e8c6c473c923d84380e444f8
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-04-11 01:28:07 +0100

    dynamic generation

---
+++ pages/static-generation/[id].js
@@ -0,0 +1,23 @@
+export default function Person({ person }) {
+  return (
+    <>
+      Hello {person.firstName} {person.lastName}!
+    </>
+  )
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+    fallback: false, // can also be true or 'blocking'
+  }
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id
+  return {
+    props: {
+      person: { firstName: "John " + id, lastName: "Doe " + id }
+    }, // will be passed to the page component as props
+  }
+}
