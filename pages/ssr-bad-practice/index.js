2798ffa3b49a9f51a57e4d458d41e624ac3f4fe5
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2023-04-23 20:16:03 +0100

    slow request in ssr

--- pages/ssr-bad-practice/index.js
+++ pages/ssr-bad-practice/index.js
@@ -4,10 +4,20 @@ const BadPractices = ({firstName, secondName}) => {
   )
 }
 
+const helper = async () => {
+  const p = new Promise((res, rej) => {
+    setTimeout(() => {
+      res({ firstName: "john", secondName: "doe"});
+    },5000)
+  });
+  return p;
+}
+
 export async function getServerSideProps(context) {
   console.log("get server props")
+  const data = await helper()
   return {
-    props: { firstName: "john", secondName: "doe"}
+    props: data
   }
 }
 
